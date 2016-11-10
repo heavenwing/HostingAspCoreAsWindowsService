8480e0e95005c98e5690262ca979850b83312958
Gavin Zhu <heavenwing@msn.com>
2016-11-10 15:49:38 +0800

    Add project files.

---
+++ src/HostingWindowsService/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+#if NET461
+using Microsoft.AspNetCore.Hosting.WindowsServices;
+#endif
+
+namespace HostingWindowsService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://*:5001")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+#if NET461
+            if (args.Contains("--windows-service"))
+            {
+                host.RunAsService();
+            }
+            else
+            {
+                host.Run();
+            }
+#else
+            host.Run();
+#endif
+        }
+    }
+}
